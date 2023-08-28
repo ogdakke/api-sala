7ccecf57d8c4d80be6dc613debe0591d7e972b57
ogdakke <daniel.wargh1@gmail.com>
2023-08-28 21:50:23 +0300

    clean up validate-secret

--- src/services/validate-secret.ts
+++ src/services/validate-secret.ts
@@ -1,19 +1,12 @@
 export function validateSecret(consumerSecret: string, suppliedSecret: string) {
-	const encoder = new TextEncoder()
-
 	const consumerUINT = new TextEncoder().encode(consumerSecret)
 	const apiSecret = new TextEncoder().encode(suppliedSecret)
 
 	if (consumerUINT.byteLength !== apiSecret.byteLength) {
-		// Strings must be the same length in order to compare
-		// with crypto.timingSafeEqual
 		return false
 	}
 
-	// The below code is vulnerable to timing attacks
-	// if (consumerUINT === apiSecret) { ... }
-
-	let equal = crypto.subtle.timingSafeEqual(consumerUINT, apiSecret)
+	const equal = crypto.subtle.timingSafeEqual(consumerUINT, apiSecret)
 
 	if (equal) {
 		// The values are equal
